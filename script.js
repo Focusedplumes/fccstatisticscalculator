28f6408e0f112e6f7f5bf9cc812148a83e49fe6e
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-05-13 07:55:17 -0500

    morning progress

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
-   
+ 
+const getMedian = (array) => {
+  const sorted = array.sort((a, b) => a - b);
+};
+
 const calculate = () => {
     const value = document.querySelector ("#numbers").value;
     const array = value.split(/,\s*/g);
     const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+    document.querySelector("#mean").textContent = mean;
     const mean = getMean(numbers);
 
 
